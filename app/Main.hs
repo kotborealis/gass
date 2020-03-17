aefd80ef717ff4fb7d7bded625de7fd8bc1d55cf
kotborealis <kotborealis@gmail.com>
2020-03-17 18:40:14 +0300

    initial commit

--- app/Main.hs
+++ app/Main.hs
@@ -2,22 +2,18 @@ module Main where
 
 import Lib
 
---import Graphics.Gloss
+import Graphics.Gloss
 
---window :: Display
---window = FullScreen
---
---renderAtom :: Atom -> Picture
---renderAtom atom = translate x y $ circle 10
---  where
---    (Atom point) = atom
---    (Point x y) = point
---
---render :: Picture
---render = pictures (map renderAtom (atoms $ initialWorld 100))
---
---main :: IO ()
---main = display window white render
+window :: Display
+window = InWindow "Gass" (800, 600) (200, 200)
+
+renderAtom :: Atom -> Picture
+renderAtom atom = translate x y $ circle 10
+  where
+    (Point x y) = position atom
+
+render :: Picture
+render = pictures (map renderAtom (atoms $ initialWorld 100))
 
 main :: IO ()
-main = putStrLn $ show $ initialWorld 100
+main = display window white render
--- src/Lib.hs
+++ src/Lib.hs
@@ -9,7 +9,10 @@ import RandomUtils
 
 data Point = Point Float Float deriving (Show, Eq)
 
-data Atom = Atom Point deriving (Show, Eq)
+data Atom = Atom
+  { position :: Point
+  , speed    :: Point
+  } deriving (Show, Eq)
 
 data World = World
   { atoms  :: [Atom]
@@ -17,8 +20,16 @@ data World = World
   , height :: Int
   } deriving (Show)
 
+normalize :: (Float, Float) -> (Float, Float) -> (Float, Float)
+normalize (xr, yr) (xd, yd) = ((xd/xr), (yd/yr))
+
 randAtoms :: (Float, Float) -> [Atom]
-randAtoms range = map (Atom . uncurry Point) $ rndPairsRs range rndGen
+randAtoms range = map (\(position, speed) ->
+      Atom { position = uncurry Point $ position, speed = uncurry Point $ normalize (10, 10) speed}
+    ) atomsData
+  where positions = rndPairsRs range rndGen
+        speeds    = rndPairsRs range rndGen
+        atomsData = zip positions speeds
 
 initialWorld :: Int -> World
 initialWorld entities =
