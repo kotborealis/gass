2c76fc62cf82ad4fb925a078b8b0362ea69d51cb
kotborealis <kotborealis@gmail.com>
2020-03-24 19:15:42 +0300

    git gud

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,23 @@
 module Main where
 
-import GasSimulation
-import System.Random
-import Control.Monad
-import Graphics.Gloss
-import Graphics.Gloss.Data.ViewPort
+import           GasSimulation
+import           System.Random
+import           Control.Monad
+import           Graphics.Gloss
+import           Graphics.Gloss.Data.ViewPort
 
 window :: Display
 window = InWindow "Gass" (800, 600) (200, 200)
 
 renderAtom :: Atom -> Picture
 renderAtom atom =
-  let (V2 x y) = position atom
-      body = translate x y $ circle 10
-      (V2 vx vy) = speed atom
-      vectorSpeed = color aquamarine $ translate x y $ line [(0, 0), (vx, vy)]
-  in pictures [ body, vectorSpeed ]
+  let
+    (V2 x y)    = atomPosition atom
+    body        = translate x y $ circle 10
+    (V2 vx vy)  = atomVelocity atom
+    vectorSpeed = color aquamarine $ translate x y $ line [(0, 0), (vx, vy)]
+  in
+    pictures [body, vectorSpeed]
 
 
 render :: World -> Picture
@@ -23,17 +25,10 @@ render (World atoms) = pictures (map renderAtom atoms)
 
 main :: IO ()
 main = do
---     let loAtom = Atom (V2 (-400) (-300)) (V2 (-10) (-10))
---     let hiAtom = Atom (V2 400 300) (V2 10 10)
---     atoms <- replicateM 200 $ getStdRandom $ randomR (loAtom, hiAtom)
-    let loAtom = Atom (V2 0 0) (V2 80 0)
-    let hiAtom = Atom (V2 120 0) (V2 0 0)
-    let atoms = [loAtom, hiAtom]
-    let world = World atoms
-    simulate
-        window
-        white
-        1
-        world
-        render
-        (\_ -> updateWorld)
+--    let loAtom = Atom (V2 (-400) (-300)) (V2 (-10) (-10))
+--    let hiAtom = Atom (V2 400 300) (V2 10 10)
+--    atoms <- replicateM 200 $ getStdRandom $ randomR (loAtom, hiAtom)
+  let loAtom = Atom (V2 0 0) (V2 10 0)
+  let hiAtom = Atom (V2 120 0) (V2 (-0) 0)
+  let world  = World [loAtom, hiAtom]
+  simulate window white 60 world render (const updateWorld)
--- src/GasSimulation.hs
+++ src/GasSimulation.hs
@@ -1,79 +1,126 @@
-{-# LANGUAGE RecordWildCards #-}
-
 module GasSimulation
-    ( V2(..),
-      Atom(..),
-      World(..),
-      updateWorld,
-    ) where
+    ( V2(..)
+    , Atom(..)
+    , World(..)
+    , updateWorld
+    )
+where
 
-import System.Random
-import Linear.Metric
-import Linear.V2
-import Linear.Vector
+import           System.Random
+import           Linear
+import           Linear.Metric
+import           Linear.V2
+import           Linear.Vector
+import           Data.List
+import           Data.Maybe
+
+-- Returns a list of 2-combinations without repetition.
+pairs xs = [ (a, b) | (a : as) <- init . tails $ xs, b <- as ]
 
 instance Random a => Random (V2 a) where
-  random g = case random g of
-   (a, g') -> case random g' of
-     (b, g'') -> (V2 a b, g'')
+    random g = case random g of
+        (a, g') -> case random g' of
+            (b, g'') -> (V2 a b, g'')
 
-  randomR (V2 a b, V2 c d) g = case randomR (a, c) g of
-    (x, g') -> case randomR (b, d) g' of
-      (y, g'') -> (V2 x y, g'')
+    randomR (V2 a b, V2 c d) g = case randomR (a, c) g of
+        (x, g') -> case randomR (b, d) g' of
+            (y, g'') -> (V2 x y, g'')
 
-atom_radius = 10
+atomRadius = 10
 
-data Atom = Atom
-  { position  :: V2 Float
-  , speed     :: V2 Float
+data Atom = Atom{
+    atomPosition  :: V2 Float,
+    atomVelocity     :: V2 Float
   } deriving (Show, Eq)
 
 instance Random Atom where
     randomR (lo, hi) g =
-        let (position', g1) = randomR (position lo, position hi) g
-            (speed',    g2) = randomR (speed lo, speed hi) g1
-        in (Atom position' speed', g2)
+        let (atomPosition', g1) = randomR (atomPosition lo, atomPosition hi) g
+            (atomVelocity', g2) = randomR (atomVelocity lo, atomVelocity hi) g1
+        in  (Atom atomPosition' atomVelocity', g2)
 
     random g =
-        let (position', g1) = random g
-            (speed',    g2) = random g1
-        in (Atom position' speed', g2)
+        let (atomPosition', g1) = random g
+            (atomVelocity', g2) = random g1
+        in  (Atom atomPosition' atomVelocity', g2)
 
-data World = World
-  { atoms  :: [Atom]
-  } deriving (Show)
+data Collision = Collision {
+    collisionAtoms :: (Atom, Atom),
+    collisionResolution :: V2 Float,
+    collisionTime :: Float
+} deriving (Eq, Show)
+
+instance Ord Collision where
+    a <= b = collisionTime a <= collisionTime b
+
+collisionAtoms_ :: Collision -> [Atom]
+collisionAtoms_ collision = [a, b] where (a, b) = collisionAtoms collision
+
+data World = World {
+    atoms  :: [Atom]
+  } deriving (Eq, Show)
 
 magnitude :: V2 Float -> Float
-magnitude (V2 x y) = sqrt $ x*x + y*y
+magnitude = distance Linear.zero
 
-checkCollision :: Float -> Atom -> Atom -> Maybe (V2 Float)
-checkCollision delta (Atom lp ls) (Atom rp rs)
-    | (distance lp rp) - sumRadii > magnitude n = Nothing
-    | d <= 0 = Nothing
-    | f >= sumRadiiSquared = Nothing
-    | t < 0 = Nothing
-    | mag < distance' = Nothing
-    | otherwise = Just (n ^* distance')
-    where
-        sumRadii = atom_radius * 2
-        sumRadiiSquared = sumRadii ** 2
-        c = lp - rp
-        mv = (ls - rs) ^* delta
-        n = normalize mv
-        d = dot n c
-        f = (magnitude c) * (magnitude c) - (d * d)
-        t = sumRadiiSquared - f
-        distance' = d - sqrt t
-        mag = magnitude mv
+collideAtoms :: Float -> Atom -> Atom -> Maybe Collision
+collideAtoms delta lha rha
+    | magnitude move < dist - sumRadii    = Nothing
+    | d <= 0                              = Nothing
+    | f >= sumRadii ** 2                  = Nothing
+    | t < 0                               = Nothing
+    | magnitude move < distUntilCollision = Nothing
+    | otherwise = Just (Collision (lha, rha) resolution time)
 
-updateAtom :: Float -> World -> Atom -> Atom
-updateAtom delta world atom = case minimum ( map (checkCollision delta atom) (atoms world) ) of
-  Just res -> atom { position = (position atom) + res }
-  Nothing -> atom { position = (position atom) + (speed atom) ^* delta }
+  where
+    move               = (atomVelocity lha - atomVelocity rha) ^* delta
+    dist               = distance (atomPosition lha) (atomPosition rha)
+    sumRadii           = atomRadius * 2
+    moveNormalized     = normalize move
+    center             = atomPosition rha - atomPosition lha
+    d                  = dot moveNormalized center
+    f                  = magnitude center ** 2 - d ** 2
+    t                  = sumRadii ** 2 - f
+    distUntilCollision = d - sqrt t
+    resolution         = moveNormalized ^* distUntilCollision
+    time               = delta * magnitude (move / resolution)
 
 
-updateAtoms :: Float -> World -> World
-updateAtoms delta world = world { atoms = map (updateAtom delta world) (atoms world) }
+calculateCollisions :: Float -> [Atom] -> [Collision]
+calculateCollisions delta atoms =
+    mapMaybe (uncurry (collideAtoms delta)) $ pairs atoms
+
+integrateAtoms :: Float -> [Atom] -> [Atom]
+integrateAtoms delta = map (integrateAtom delta)
+
+integrateAtom :: Float -> Atom -> Atom
+integrateAtom 0     atom = atom
+integrateAtom delta atom = atom { atomPosition = atomPosition'' }
+  where
+    atomPosition'  = atomPosition atom
+    atomVelocity'  = atomVelocity atom
+    atomPosition'' = atomPosition' + (atomVelocity' ^* delta)
+
+runPhysics :: Float -> [Atom] -> [Atom]
+runPhysics 0 atoms = atoms
+runPhysics delta atoms | null collisions = integrateAtoms delta atoms
+                       | tFirst > delta  = integrateAtoms delta atoms
+                       | otherwise       = runPhysics delta' atoms'
+
+  where
+    collisions        = calculateCollisions delta atoms
+    firstCollision    = minimum collisions
+    tFirst            = collisionTime firstCollision
+    tFirst'           = tFirst - 1.0e-3
+    delta'            = delta - tFirst'
+    nonCollidingAtoms = atoms \\ collisionAtoms_ firstCollision
+    atoms' =
+        integrateAtoms tFirst' nonCollidingAtoms
+            ++ resolveCollision tFirst' firstCollision
+
+resolveCollision :: Float -> Collision -> [Atom]
+resolveCollision delta collision = [a, b]
+    where (a, b) = collisionAtoms collision
 
 updateWorld :: Float -> World -> World
-updateWorld delta world = updateAtoms delta world
+updateWorld delta world = world { atoms = runPhysics delta (atoms world) }
